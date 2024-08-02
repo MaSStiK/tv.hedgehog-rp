d3c7602e8c749c5fe0638a0f7cf194045f947a7a
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-08-02 20:48:14 +0200

    css

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@
 import ReactPlayer from "react-player";
 import { useState, useEffect } from "react";
 
+import "./styles/video.css";
+
 export default function Home() {
     let videoS4E5 = "/videos/s4e5.mp4"
     let videoS4E4 = "/videos/s4e4.mp4"
@@ -21,39 +23,47 @@ export default function Home() {
                 <>
                     <section>
                         <h1>Сезон 4</h1>
-                        <div className="flex flex-row">
-                            <ReactPlayer
-                                url={videoS4E5}
-                                controls={true}
-                                pip={true}
-                            />
-                            <source src={videoS4E5} type="video/mp4" />
+                        <div className="flex flex-row video-row">
+                            <div className="video-player">
+                                <ReactPlayer
+                                    url={videoS4E5}
+                                    controls={true}
+                                    pip={true}
+                                />
+                                <source src={videoS4E5} type="video/mp4" />
+                            </div>
 
-                            <ReactPlayer
-                                url={videoS4E4}
-                                controls={true}
-                                pip={true}
-                            />
-                            <source src={videoS4E4} type="video/mp4" />
+                            <div className="video-player">
+                                <ReactPlayer
+                                    url={videoS4E4}
+                                    controls={true}
+                                    pip={true}
+                                />
+                                <source src={videoS4E4} type="video/mp4" />
+                            </div>
 
-                            <ReactPlayer
-                                url={videoS4E3}
-                                controls={true}
-                                pip={true}
-                            />
-                            <source src={videoS4E3} type="video/mp4" />
+                            <div className="video-player">
+                                <ReactPlayer
+                                    url={videoS4E3}
+                                    controls={true}
+                                    pip={true}
+                                />
+                                <source src={videoS4E3} type="video/mp4" />
+                            </div>
                         </div>
                     </section>
 
                     <section className="mt-[20px]">
                         <h1>work in progress</h1>
-                        <div className="flex flex-row">
-                            <ReactPlayer
-                                url={videoDemo}
-                                controls={true}
-                                pip={true} // picture in picture
-                            />
-                            <source src={videoDemo} type="video/mp4" />
+                        <div className="flex flex-row video-row">
+                            <div className="video-player">
+                                <ReactPlayer
+                                    url={videoDemo}
+                                    controls={true}
+                                    pip={true} // picture in picture
+                                />
+                                <source src={videoDemo} type="video/mp4" />
+                            </div>
                         </div>
                     </section>
                 </>

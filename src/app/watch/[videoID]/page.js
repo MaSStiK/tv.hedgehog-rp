ee684a5ca62f6221b2154a02ff87cb6770a3b912
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-08-04 14:15:55 +0200

    video title

--- src/app/watch/[videoID]/page.js
+++ src/app/watch/[videoID]/page.js
@@ -8,6 +8,7 @@ import style from "./watch.module.css";
 
 export default function Watch() {
     const [VideoSrc, setVideoSrc] = useState()
+    const [VideoTitle, setVideoTitle] = useState("")
     const params = useParams()
 
     useEffect(() => {
@@ -15,12 +16,14 @@ export default function Watch() {
         let videoSeason = videoID.split("e")[0] // Сезон видео
         let videoIndex = seasons[videoSeason].videos.findIndex(el => el.videoID === videoID)
         setVideoSrc(seasons[videoSeason].videos[videoIndex].src)
+        setVideoTitle(seasons[videoSeason].videos[videoIndex].title)
     }, [params]);
 
      return (
         <article>
             <div className={style.playerWrapper}>
                 <iframe className={style.player} src={VideoSrc}></iframe>
+                <h3>{VideoTitle}</h3>
             </div>
         </article>
     )

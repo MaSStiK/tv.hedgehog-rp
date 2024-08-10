29975500853aeaa5b30ffdf1415a4f30ccc2d2c5
MaSStiK <matvey.ovchinnikov.2014@mail.ru>
2024-08-10 20:55:57 +0200

    thumbnail

--- src/components/seasons.js
+++ src/components/seasons.js
@@ -1,39 +1,39 @@
-import imgS4E8 from "@/assets/thumbnails/S4E8.png"
-import imgS4E7 from "@/assets/thumbnails/S4E7.png"
-import imgS4E6 from "@/assets/thumbnails/S4E6.png"
-import imgS4E5 from "@/assets/thumbnails/S4E5.png"
-import imgS4E4 from "@/assets/thumbnails/S4E4.png"
-import imgS4E3 from "@/assets/thumbnails/S4E3.png"
-import imgS4E2 from "@/assets/thumbnails/S4E2.png"
-import imgS4E1 from "@/assets/thumbnails/S4E1.png"
+import thumbnailS4E8 from "@/assets/thumbnails/S4E8.png"
+import thumbnailS4E7 from "@/assets/thumbnails/S4E7.png"
+import thumbnailS4E6 from "@/assets/thumbnails/S4E6.png"
+import thumbnailS4E5 from "@/assets/thumbnails/S4E5.png"
+import thumbnailS4E4 from "@/assets/thumbnails/S4E4.png"
+import thumbnailS4E3 from "@/assets/thumbnails/S4E3.png"
+import thumbnailS4E2 from "@/assets/thumbnails/S4E2.png"
+import thumbnailS4E1 from "@/assets/thumbnails/S4E1.png"
 
-import imgS3E10 from "@/assets/thumbnails/S3E10.png"
-import imgS3E9 from "@/assets/thumbnails/S3E9.png"
-import imgS3E8 from "@/assets/thumbnails/S3E8.png"
-import imgS3E7 from "@/assets/thumbnails/S3E7.png"
-import imgS3E6 from "@/assets/thumbnails/S3E6.png"
-import imgS3E5 from "@/assets/thumbnails/S3E5.png"
-import imgS3E4 from "@/assets/thumbnails/S3E4.png"
-import imgS3E3 from "@/assets/thumbnails/S3E3.png"
-import imgS3E2 from "@/assets/thumbnails/S3E2.png"
-import imgS3E1 from "@/assets/thumbnails/S3E1.png"
+import thumbnailS3E10 from "@/assets/thumbnails/S3E10.png"
+import thumbnailS3E9 from "@/assets/thumbnails/S3E9.png"
+import thumbnailS3E8 from "@/assets/thumbnails/S3E8.png"
+import thumbnailS3E7 from "@/assets/thumbnails/S3E7.png"
+import thumbnailS3E6 from "@/assets/thumbnails/S3E6.png"
+import thumbnailS3E5 from "@/assets/thumbnails/S3E5.png"
+import thumbnailS3E4 from "@/assets/thumbnails/S3E4.png"
+import thumbnailS3E3 from "@/assets/thumbnails/S3E3.png"
+import thumbnailS3E2 from "@/assets/thumbnails/S3E2.png"
+import thumbnailS3E1 from "@/assets/thumbnails/S3E1.png"
 
-import imgS2E7 from "@/assets/thumbnails/S2E7.png"
-import imgS2E6 from "@/assets/thumbnails/S2E6.png"
-import imgS2E5 from "@/assets/thumbnails/S2E5.png"
-import imgS2E4 from "@/assets/thumbnails/S2E4.png"
-import imgS2E3 from "@/assets/thumbnails/S2E3.png"
-import imgS2E2 from "@/assets/thumbnails/S2E2.png"
-import imgS2E1 from "@/assets/thumbnails/S2E1.png"
+import thumbnailS2E7 from "@/assets/thumbnails/S2E7.png"
+import thumbnailS2E6 from "@/assets/thumbnails/S2E6.png"
+import thumbnailS2E5 from "@/assets/thumbnails/S2E5.png"
+import thumbnailS2E4 from "@/assets/thumbnails/S2E4.png"
+import thumbnailS2E3 from "@/assets/thumbnails/S2E3.png"
+import thumbnailS2E2 from "@/assets/thumbnails/S2E2.png"
+import thumbnailS2E1 from "@/assets/thumbnails/S2E1.png"
 
-import imgS1E8 from "@/assets/thumbnails/S1E8.png"
-import imgS1E7 from "@/assets/thumbnails/S1E7.png"
-import imgS1E6 from "@/assets/thumbnails/S1E6.png"
-import imgS1E5 from "@/assets/thumbnails/S1E5.png"
-import imgS1E4 from "@/assets/thumbnails/S1E4.png"
-import imgS1E3 from "@/assets/thumbnails/S1E3.png"
-import imgS1E2 from "@/assets/thumbnails/S1E2.png"
-import imgS1E1 from "@/assets/thumbnails/S1E1.png"
+import thumbnailS1E8 from "@/assets/thumbnails/S1E8.png"
+import thumbnailS1E7 from "@/assets/thumbnails/S1E7.png"
+import thumbnailS1E6 from "@/assets/thumbnails/S1E6.png"
+import thumbnailS1E5 from "@/assets/thumbnails/S1E5.png"
+import thumbnailS1E4 from "@/assets/thumbnails/S1E4.png"
+import thumbnailS1E3 from "@/assets/thumbnails/S1E3.png"
+import thumbnailS1E2 from "@/assets/thumbnails/S1E2.png"
+import thumbnailS1E1 from "@/assets/thumbnails/S1E1.png"
 
 const seasons = {
     s4: {
@@ -42,56 +42,56 @@ const seasons = {
             // {
             //     title: "Выйдет 12.08 в 15:00 по МСК",
             //     // title: "4 сезон 9 серия | Название не придумано",
-            //     thumbnail: imgS4E9,
+            //     thumbnail: thumbnailS4E9,
             //     videoID: "s4e9",
             //     src: ""
             // },
             {
                 // title: "Выйдет 09.08 в 15:00 по МСК",
                 title: "4 сезон 8 серия | Суета",
-                thumbnail: imgS4E8,
+                thumbnail: thumbnailS4E8,
                 videoID: "s4e8",
                 src: "https://drive.google.com/file/d/10Gw7CB63kcwdVSOCBgmmFHvFTh_M5vzO/preview"
             },
             {
                 title: "4 сезон 7 серия | Напряжение у западных морей",
-                thumbnail: imgS4E7,
+                thumbnail: thumbnailS4E7,
                 videoID: "s4e7",
                 src: "https://drive.google.com/file/d/1WaNMI1V_O7-MTPdxbAe9UiUk2JxpT-0g/preview"
             },
             {
                 title: "4 сезон 6 серия | Лефанляндский вопрос",
-                thumbnail: imgS4E6,
+                thumbnail: thumbnailS4E6,
                 videoID: "s4e6",
                 src: "https://drive.google.com/file/d/1QZqfQENrVqQPA23sdJH0nB6b6Kepafwm/preview"
             },
             {
                 title: "4 сезон 5 серия | Коллапс",
-                thumbnail: imgS4E5,
+                thumbnail: thumbnailS4E5,
                 videoID: "s4e5",
                 src: "https://drive.google.com/file/d/1hnti2vqzvixmZC3W6HdZjBwmP51AikAj/preview"
             },
             {
                 title: "4 сезон 4 серия | Хиенспатт",
-                thumbnail: imgS4E4,
+                thumbnail: thumbnailS4E4,
                 videoID: "s4e4",
                 src: "https://drive.google.com/file/d/1aSTjPc7-X-vuUiGYgmmyAh20czQdPmOY/preview"
             },
             {
                 title: "4 сезон 3 серия | Осваиваемся",
-                thumbnail: imgS4E3,
+                thumbnail: thumbnailS4E3,
                 videoID: "s4e3",
                 src: "https://drive.google.com/file/d/1KU_HjAQEyRZaq2TqWdlYzVdsSi4fboN-/preview"
             },
             {
                 title: "4 сезон 2 серия | 2016, Новый Свет и Анастасия",
-                thumbnail: imgS4E2,
+                thumbnail: thumbnailS4E2,
                 videoID: "s4e2",
                 src: "https://drive.google.com/file/d/1Ybk6JZSo2l77SU4WkMBe67dysGnJr2Bi/preview"
             },
             {
                 title: "4 сезон 1 серия | Спокойные года",
-                thumbnail: imgS4E1,
+                thumbnail: thumbnailS4E1,
                 videoID: "s4e1",
                 src: "https://drive.google.com/file/d/1tSkwfiZTRGhKBV2UUshHTVQYPMTc_BE4/preview"
             }
@@ -102,61 +102,61 @@ const seasons = {
         videos: [
             {
                 title: "3 сезон 10 серия | XIV век",
-                thumbnail: imgS3E10,
+                thumbnail: thumbnailS3E10,
                 videoID: "s3e10",
                 src: "https://drive.google.com/file/d/1Ul2FE-sQMJ4HFmSxXruAu4QZFlB6ox1X/preview"
             },
             {
                 title: "3 сезон 9 серия | Всё точно приходит в норму",
-                thumbnail: imgS3E9,
+                thumbnail: thumbnailS3E9,
                 videoID: "s3e9",
                 src: "https://drive.google.com/file/d/1_zcF1DYcVnvVolDkQ5ovwgMxCZXKVL0n/preview"
             },
             {
                 title: "3 сезон 8 серия | От вторжения к вторжению",
-                thumbnail: imgS3E8,
+                thumbnail: thumbnailS3E8,
                 videoID: "s3e8",
                 src: "https://drive.google.com/file/d/1zvLkPlpEltwkszbv-0TrapebvqrZLZUV/preview"
             },
             {
                 title: "3 сезон 7 серия | Потерянный Эрикград, Рафталия и Владо-Никитский Раскол",
-                thumbnail: imgS3E7,
+                thumbnail: thumbnailS3E7,
                 videoID: "s3e7",
                 src: "https://drive.google.com/file/d/1OC4_cEDX-sk2THAdR9f0wwod_ZkxR2LU/preview"
             },
             {
                 title: "3 сезон 6 серия | Кошмар",
-                thumbnail: imgS3E6,
+                thumbnail: thumbnailS3E6,
                 videoID: "s3e6",
                 src: "https://drive.google.com/file/d/1BMuRr3WyT0Qk0C7DLzTTUFHF9V2xmp5b/preview"
             },
             {
                 title: "3 сезон 5 серия | Начало Нового Возрождения",
-                thumbnail: imgS3E5,
+                thumbnail: thumbnailS3E5,
                 videoID: "s3e5",
                 src: "https://drive.google.com/file/d/1m9mvxg_y87wNyvsVedZ7gtTRNqQbD-pw/preview"
             },
             {
                 title: "3 сезон 4 серия | Конец близко",
-                thumbnail: imgS3E4,
+                thumbnail: thumbnailS3E4,
                 videoID: "s3e4",
                 src: "https://drive.google.com/file/d/1Vboj8mTB0AgSdxcp8w5xaiLOP35cnj4I/preview"
             },
             {
                 title: "3 сезон 3 серия | Проблемы обоих сторон",
-                thumbnail: imgS3E3,
+                thumbnail: thumbnailS3E3,
                 videoID: "s3e3",
                 src: "https://drive.google.com/file/d/1n0vuzcRBt90OtVbZlfgT50K4D2XUGbt3/preview"
             },
             {
                 title: "3 сезон 2 серия | Кулсториробоб навсегда",
-                thumbnail: imgS3E2,
+                thumbnail: thumbnailS3E2,
                 videoID: "s3e2",
                 src: "https://drive.google.com/file/d/1pQlbFwKoFbm5y6MyK0whtJfZN0hK7OCc/preview"
             },
             {
                 title: "3 сезон 1 серия | И всё спокойнее становится жить... | 1 - 13 декабря",
-                thumbnail: imgS3E1,
+                thumbnail: thumbnailS3E1,
                 videoID: "s3e1",
                 src: "https://drive.google.com/file/d/1pkVzWc46lOo2YAwMAUz9i9Up8yT05bbz/preview"
             }
@@ -167,43 +167,43 @@ const seasons = {
         videos: [
             {
                 title: "2 сезон 7 серия | Полный конец",
-                thumbnail: imgS2E7,
+                thumbnail: thumbnailS2E7,
                 videoID: "s2e7",
                 src: "https://drive.google.com/file/d/1FxHj5FhiF1lxnjLJZsJj8-sSmPfExRiC/preview"
             },
             {
                 title: "2 сезон 6 серия | Конец троевластия - часть вторая",
-                thumbnail: imgS2E6,
+                thumbnail: thumbnailS2E6,
                 videoID: "s2e6",
                 src: "https://drive.google.com/file/d/13NU1iLVQmbn5evR9VDzQVZtBxvuV0InZ/preview"
             },
             {
                 title: "2 сезон 5 серия | Конец троевластия - часть первая",
-                thumbnail: imgS2E5,
+                thumbnail: thumbnailS2E5,
                 videoID: "s2e5",
                 src: "https://drive.google.com/file/d/1zly9j8foWiAkwHAvmw0CHGo2_uG0a1zt/preview"
             },
             {
                 title: "2 сезон 4 серия | И разрушения пошли по планете..",
-                thumbnail: imgS2E4,
+                thumbnail: thumbnailS2E4,
                 videoID: "s2e4",
                 src: "https://drive.google.com/file/d/13jfBtFWlT8lgfOop9BBhoiFsWl6T5lfJ/preview"
             },
             {
                 title: "2 сезон 3 серия | Все сложно",
-                thumbnail: imgS2E3,
+                thumbnail: thumbnailS2E3,
                 videoID: "s2e3",
                 src: "https://drive.google.com/file/d/1-wkJVqNjjHv6bSyEi5z4mG5IDDm_J1mL/preview"
             },
             {
                 title: "2 сезон 2 серия | Неспокойное десятилетие",
-                thumbnail: imgS2E2,
+                thumbnail: thumbnailS2E2,
                 videoID: "s2e2",
                 src: "https://drive.google.com/file/d/1fOWfx8Wezcqwf0u13hgwShihYp1uWnIu/preview"
             },
             {
                 title: "2 сезон 1 серия | Новый мир",
-                thumbnail: imgS2E1,
+                thumbnail: thumbnailS2E1,
                 videoID: "s2e1",
                 src: "https://drive.google.com/file/d/1uzj3_ZdB7g-6g1bY7R-L2sLwelz3xKrT/preview"
             }
@@ -214,49 +214,49 @@ const seasons = {
         videos: [
             {
                 title: "1 сезон 8 серия | Переход к новому миру",
-                thumbnail: imgS1E8,
+                thumbnail: thumbnailS1E8,
                 videoID: "s1e8",
                 src: "https://drive.google.com/file/d/1bOJBjkutV_WNWf4R4cV0q1WUFsMT-ZmS/preview"
             },
             {
-                title: "1 cезон 7 серия | Сложное десятилетие",
-                thumbnail: imgS1E7,
+                title: "1 сезон 7 серия | Сложное десятилетие",
+                thumbnail: thumbnailS1E7,
                 videoID: "s1e7",
                 src: "https://drive.google.com/file/d/1icbQUV_iHX8BkoWeAMrvyynOeSwOUjlK/preview"
             },
             {
                 title: "1 сезон 6 серия | Жизнь после смерти",
-                thumbnail: imgS1E6,
+                thumbnail: thumbnailS1E6,
                 videoID: "s1e6",
                 src: "https://drive.google.com/file/d/1PUfWvyw8eXE7zBA5J8uDoAqZcQwL78KF/preview"
             },
             {
                 title: "1 сезон 5 серия | Продолжение медвежьего конца",
-                thumbnail: imgS1E5,
+                thumbnail: thumbnailS1E5,
                 videoID: "s1e5",
                 src: "https://drive.google.com/file/d/1-rjefdCSoiUnnK6CoCx3jZ8P1Ionmt5w/preview"
             },
             {
                 title: "1 сезон 4 серия | Начало медвежьего конца",
-                thumbnail: imgS1E4,
+                thumbnail: thumbnailS1E4,
                 videoID: "s1e4",
                 src: "https://drive.google.com/file/d/1PXfKD24QIsVrRvLtK3J2i2L-SxVqVW6l/preview"
             },
             {
                 title: "1 сезон 3 серия | Затишье перед бурей",
-                thumbnail: imgS1E3,
+                thumbnail: thumbnailS1E3,
                 videoID: "s1e3",
                 src: "https://drive.google.com/file/d/1t84xQ5R-gvo5LxUMy3ZqWQ-rk_XRMwX_/preview"
             },
             {
                 title: "1 сезон 2 серия | Павшие муравьи",
-                thumbnail: imgS1E2,
+                thumbnail: thumbnailS1E2,
                 videoID: "s1e2",
                 src: "https://drive.google.com/file/d/1aoIlCokdUOIr4wfBA5N3SznaRDb0OPO6/preview"
             },
             {
                 title: "1 сезон 1 серия | iПилот",
-                thumbnail: imgS1E1,
+                thumbnail: thumbnailS1E1,
                 videoID: "s1e1",
                 src: "https://drive.google.com/file/d/1faTcepLwVFnjrcMwk9AHKB_KBzLL8fMd/preview"
             }

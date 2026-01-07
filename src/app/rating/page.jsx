6c700d8c5de8c8d71846d1cd70bf6fbb1be144d9
MaSStiK <mathew.ovchinnikov@gmail.com>
2026-01-07 23:45:28 +0100

    change reviews to rating

--- src/app/rating/page.jsx
+++ src/app/rating/page.jsx
@@ -1,7 +1,7 @@
 import AboutProject from "@/components/AboutProject/AboutProject"
 import Reviews from "@/components/Reviews/Reviews"
 
-export default function ReviewsPage() {
+export default function Rating() {
     return (
         <main>
             <AboutProject withoutLink={true} />

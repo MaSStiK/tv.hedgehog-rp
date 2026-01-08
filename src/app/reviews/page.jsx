4af591114b51a54cc521b3c0ca9d7f20ec5c6def
MaSStiK <mathew.ovchinnikov@gmail.com>
2026-01-08 01:34:23 +0100

    added back withoutLink

--- src/app/reviews/page.jsx
+++ src/app/reviews/page.jsx
@@ -4,7 +4,7 @@ import Reviews from "@/components/Reviews/Reviews"
 export default function Rating() {
     return (
         <main>
-            <AboutProject />
+            <AboutProject withoutLink={true} />
             <Reviews />
         </main>
     )
